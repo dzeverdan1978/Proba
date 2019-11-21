e12727ae7e8f66b03102710d8ea852a0558f5c36
dzeverdan1978 <nenad.dzeverdanovic@outlook.com>
2019-11-21 09:23:19 +0100

    Ibitial

---
+++ BinarySearchDemo/BinarySearchHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchDemo
+{
+    public static class BinarySearchHelper
+    {
+        public static int BinarySearchIterative(int[] inputArray, int key)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        public static int BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            try
+            {
+                if (min > max)
+                {
+                    return -2;
+                }
+                else
+                {
+                    int mid = (min + max) / 2;
+                    if (key == inputArray[mid])
+                    {
+                        return ++mid;
+                    }
+                    else if (key < inputArray[mid])
+                    {
+                        return BinarySearchRecursive(inputArray, key, min, mid - 1);
+                    }
+                    else
+                    {
+                        return BinarySearchRecursive(inputArray, key, mid + 1, max);
+                    }
+                }
+            }
+            catch
+            {
+                return -3;
+            }
+        }
+    }
+}
---
+++ BinarySearchDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BinarySearchDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] {0, 1, 10, 23, 34, 45, 67};
+            Console.WriteLine("Non recursive");
+            Console.Write("Enter the search key:");
+            int key = int.Parse(Console.ReadLine());
+            int resIterative = -1;
+            
+            resIterative= BinarySearchHelper.BinarySearchIterative(input, key);
+            if (resIterative>=0)
+            {
+                Console.WriteLine("Result is " + resIterative);
+            }
+            else
+            {
+                Console.WriteLine("Invalid key value");
+            }
+
+            Console.WriteLine("Recursive");
+            Console.Write("Enter the search key:");
+            key = int.Parse(Console.ReadLine());
+            Console.Write("Enter the search min value:");
+            int min = int.Parse(Console.ReadLine());
+            Console.Write("Enter the search max value:");
+            int max = int.Parse(Console.ReadLine());
+            int resRecursive = -1;
+
+            resRecursive= BinarySearchHelper.BinarySearchRecursive(input, key, min, max);
+
+            if (resRecursive>=0)
+            {
+                Console.WriteLine("Result is " + resRecursive);
+            }
+            else
+            {
+                if (resRecursive==-2)
+                    Console.WriteLine("Invalid min and max values");
+
+                if (resRecursive == -3)
+                    Console.WriteLine("Index out of range of the array");
+
+            }
+
+            
+
+;        }
+    }
+}
